4440cdab8e8108efb074b2c15e993a5826a4ed10
sumaiya aftab <129832134+prova116@users.noreply.github.com>
2025-09-02 19:57:07 +0600

    Added navbar

---
+++ CrystalCarCare/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace CrystalCarCare.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /User/BookingStatus
+        public ActionResult BookingStatus()
+        {
+            return View(); // Views/User/BookingStatus.cshtml
+        }
+
+        // You can add other user pages here (Profile, History, etc.)
+    }
+}
