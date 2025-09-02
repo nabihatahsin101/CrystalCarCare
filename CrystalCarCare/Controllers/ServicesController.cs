5a09ff6c43554bedee520d454ac9829adffcf6bd
sumaiya aftab <129832134+prova116@users.noreply.github.com>
2025-09-02 19:57:07 +0600

    Added navbar

---
+++ CrystalCarCare/Controllers/ServicesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace CrystalCarCare.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public ActionResult CarWash()  // Make sure the name matches the view
+        {
+            return View();
+        }
+
+        public ActionResult Mechanic()
+        {
+            return View();
+        }
+
+        public ActionResult Additional()
+        {
+            return View();
+        }
+    }
+}
