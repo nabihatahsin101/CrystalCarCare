f3e07d33bbff6ff9815fe183c9ad32d7162650e1
srabanimitra <srabanimitra2222@gmail.com>
2025-09-03 00:31:49 +0600

    login signup with database

---
+++ CrystalCarCare/Migrations/202509021704551_InitialCreate.cs
@@ -0,0 +1,16 @@
+﻿namespace CrystalCarCare.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
