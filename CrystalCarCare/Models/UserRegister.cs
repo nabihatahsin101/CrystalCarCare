1b1a0df5b0a853d836b0ff3d36eccec0c19a764d
srabanimitra <srabanimitra2222@gmail.com>
2025-09-03 00:31:49 +0600

    login signup with database

---
+++ CrystalCarCare/Models/UserRegister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrystalCarCare.Models
+{
+    public class UserRegister
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
