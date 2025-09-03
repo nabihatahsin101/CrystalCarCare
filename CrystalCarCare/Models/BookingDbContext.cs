455f3c63d6a44ad504d90337c1b11635d8badca4
srabanimitra <srabanimitra2222@gmail.com>
2025-09-04 00:43:03 +0600

    booking

---
+++ CrystalCarCare/Models/BookingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace CrystalCarCare.Models
+{
+    public class BookingDbContext : DbContext
+    {
+        public BookingDbContext() : base("UserDbContext") { }
+
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
