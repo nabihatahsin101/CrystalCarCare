dca3a4307e3c06d3413643dc31c9f52b9dded02a
Adiba <nusratadiba06@gmail.com>
2025-09-24 04:51:41 +0600

    admin1

---
+++ CrystalCarCare/Models/AdminLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalCarCare.Models
+{
+    public class AdminLoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
