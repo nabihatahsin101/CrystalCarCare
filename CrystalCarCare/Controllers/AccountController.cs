3b740d2627bb51ce77779b6d4647662f25317ab4
srabanimitra <srabanimitra2222@gmail.com>
2025-09-24 00:32:29 +0600

    booking confirmation ,pdf generator

--- CrystalCarCare/Controllers/AccountController.cs
+++ CrystalCarCare/Controllers/AccountController.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Web;
 
 namespace CrystalCarCare.Controllers
 {
@@ -79,15 +83,60 @@ namespace CrystalCarCare.Controllers
                 db.Bookings.Add(booking);
                 db.SaveChanges();
                 TempData["Message"] = "Your booking has been successfully submitted!";
-                return RedirectToAction("BookingConfirmation");
+                return RedirectToAction("BookingConfirmation", new { bookingId = booking.BookingId });
+
             }
-            
+
             return View(booking);
         }
 
-        public ActionResult BookingConfirmation()
+        public ActionResult BookingConfirmation(int bookingId)
         {
-            return View();
+            var booking = db.Bookings.FirstOrDefault(b => b.BookingId == bookingId);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+
+            var user = db.Users.FirstOrDefault(u => u.Email == User.Identity.Name);
+
+            var model = new BookingConfirmationViewModel
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                Booking = booking
+            };
+
+            return View(model);
+        }
+
+        // 🔹 PDF Generator
+        public FileResult DownloadConfirmation(int bookingId)
+        {
+            var booking = db.Bookings.FirstOrDefault(b => b.BookingId == bookingId);
+            var user = db.Users.FirstOrDefault(u => u.Email == User.Identity.Name);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Document doc = new Document();
+                PdfWriter.GetInstance(doc, ms);
+                doc.Open();
+
+                doc.Add(new Paragraph("Booking Confirmation"));
+                doc.Add(new Paragraph($"Name: {user.UserName}"));
+                doc.Add(new Paragraph($"Email: {user.Email}"));
+                doc.Add(new Paragraph($"Service Type: {booking.ServiceType}"));
+                doc.Add(new Paragraph($"Date: {booking.Date.ToShortDateString()}"));
+                doc.Add(new Paragraph($"Time: {booking.Time}"));
+                doc.Add(new Paragraph($"Vehicle: {booking.VehicleYear} {booking.VehicleMake} {booking.VehicleModel}"));
+                doc.Add(new Paragraph($"License Plate: {booking.LicensePlate}"));
+                if (!string.IsNullOrEmpty(booking.SpecialRequests))
+                    doc.Add(new Paragraph($"Special Requests: {booking.SpecialRequests}"));
+
+                doc.Close();
+
+                return File(ms.ToArray(), "application/pdf", "BookingConfirmation.pdf");
+            }
         }
 
 
